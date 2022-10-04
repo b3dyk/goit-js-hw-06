c9c7ca77adf11fcdcb5fe906c2b88d0bd4faa50e
Oleksandr Perlov <oleksandrperlov@gmail.com>
2022-10-04 12:48:56 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+let html = "";
+
+ingredients.forEach((ingredient) => {
+  html += `<li class="item">${ingredient}</li>`;
+});
+
+const ingredientsListRef = document.querySelector("#ingredients");
+
+ingredientsListRef.innerHTML = html;
+
+console.log(ingredientsListRef);
