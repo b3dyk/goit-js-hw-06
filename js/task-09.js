b5682fef0b3946a5317bb195b40309b21e97eec2
Oleksandr Perlov <oleksandrperlov@gmail.com>
2022-10-06 23:18:37 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const colorNameRef = document.querySelector(".color");
+const changeColorBtnRef = document.querySelector(".change-color");
+const bodyRef = document.querySelector("body");
+
+changeColorBtnRef.addEventListener("click", () => {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  colorNameRef.textContent = getRandomHexColor();
+});
