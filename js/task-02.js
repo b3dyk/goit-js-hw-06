1a0765f655c0b2749d9486539988b806f92c3d47
Oleksandr Perlov <oleksandrperlov@gmail.com>
2022-10-07 23:37:53 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -7,14 +7,29 @@ const ingredients = [
   "Condiments",
 ];
 
-let html = "";
-
-ingredients.forEach((ingredient) => {
-  html += `<li class="item">${ingredient}</li>`;
-});
-
 const ingredientsListRef = document.querySelector("#ingredients");
 
-ingredientsListRef.innerHTML = html;
+function createElement(text) {
+  const li = document.createElement("li");
 
-console.log(ingredientsListRef);
+  li.classList.add("item");
+  li.textContent = text;
+
+  return li;
+}
+
+const list = ingredients.map((ingredient) => {
+  return createElement(ingredient);
+});
+
+ingredientsListRef.append(...list);
+
+// 2-й варіант================================================================
+
+// let html = "";
+
+// ingredients.forEach((ingredient) => {
+//   html += `<li class="item">${ingredient}</li>`;
+// });
+
+// ingredientsListRef.innerHTML = html;
